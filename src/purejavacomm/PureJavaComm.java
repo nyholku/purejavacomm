05340dac1e1bc589f31e23ca5c50d07c0cf862ae
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2011-11-07 09:40:02 +0200

    Bumped version number to celebrate maven deployment

--- src/purejavacomm/PureJavaComm.java
+++ src/purejavacomm/PureJavaComm.java
@@ -3,10 +3,10 @@ package purejavacomm;
 public class PureJavaComm {
 	// change this if you modify the code and re-distribute
 	public static String getVersion() {
-		return "0.0.0.1";
+		return "0.0.1";
 	}
 
-	// change this is you fork the project
+	// change this is if you fork the project
 	public static String getFork() {
 		return "SpareTimeLabs";
 	}
