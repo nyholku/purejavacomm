1ba8f23306e13acba6c9efdf067ed2d600eac80b
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2012-11-19 10:45:11 +0200

    Small optimization by removing constant stuff to initialization

--- src/purejavacomm/PureJavaSerialPort.java
+++ src/purejavacomm/PureJavaSerialPort.java
@@ -584,6 +584,8 @@ public class PureJavaSerialPort extends SerialPort {
 					m_ReadFDSet = newFDSet();
 					m_ReadTimeVal = new TimeVal();
 					m_ReadPollFD = new int[4];
+					m_ReadPollFD[0] = m_FD;
+					m_ReadPollFD[2] = m_PipeRdFD;
 					m_Nudge = new byte[1];
 				}
 
@@ -674,9 +676,7 @@ public class PureJavaSerialPort extends SerialPort {
 							// called when no data is available
 							// so that we will not hang for ever in a read
 							if (USE_POLL) {
-								m_ReadPollFD[0] = m_FD;
 								m_ReadPollFD[1] = POLLIN_IN;
-								m_ReadPollFD[2] = m_PipeRdFD;
 								m_ReadPollFD[3] = POLLIN_IN;
 								int n;
 								if (m_HaveNudgePipe)
