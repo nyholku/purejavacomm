9ebdafb84bbc8210d6c47c050b5f7db2015df097
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2013-10-28 17:13:48 +0200

    Changed version number to 0.0.18

--- src/purejavacomm/PureJavaComm.java
+++ src/purejavacomm/PureJavaComm.java
@@ -3,7 +3,7 @@ package purejavacomm;
 public class PureJavaComm {
 	// change this if you modify the code and re-distribute
 	public static String getVersion() {
-		return "0.0.17";
+		return "0.0.18";
 	}
 
 	// change this is if you fork the project
