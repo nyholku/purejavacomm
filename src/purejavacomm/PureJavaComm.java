d04e3ed430c99f1b0333ac516aab6458251c9397
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2015-09-04 07:09:14 +0300

    Version 0.0.25

--- src/purejavacomm/PureJavaComm.java
+++ src/purejavacomm/PureJavaComm.java
@@ -3,7 +3,7 @@ package purejavacomm;
 public class PureJavaComm {
 	// change this if you modify the code and re-distribute
 	public static String getVersion() {
-		return "0.0.24";
+		return "0.0.25";
 	}
 
 	// change this is if you fork the project
