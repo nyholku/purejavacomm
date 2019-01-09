61a21cf8f4eb87f44a1b5f372b2bf29758abfeaf
nyholku <kustaa.nyholm@sparetimelabs.com>
2019-01-09 19:18:13 +0200

    Change version number

--- src/purejavacomm/PureJavaComm.java
+++ src/purejavacomm/PureJavaComm.java
@@ -3,7 +3,7 @@ package purejavacomm;
 public class PureJavaComm {
 	// change this if you modify the code and re-distribute
 	public static String getVersion() {
-		return "1.0.1";
+		return "1.0.2";
 	}
 
 	// change this is if you fork the project
